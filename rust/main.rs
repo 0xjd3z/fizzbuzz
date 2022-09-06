f763f50d58251fb2256c2cf8ca32f422871461f7
0xjdez <jimroxodezi@gmail.com>
2022-09-06 03:39:03 +0100

    rust

---
+++ rust/main.rs
@@ -0,0 +1,15 @@
+
+
+fn main(){
+    for num in 1..100 {
+        if num % 15 == 0 {
+            println!("fizzbuzz")
+        }else if num % 3 == 0 {
+            println!("fizz")
+        }else if num % 5 == 0 {
+            println!("buzz")
+        }else{
+            println!(num)
+        }
+    }
+}
